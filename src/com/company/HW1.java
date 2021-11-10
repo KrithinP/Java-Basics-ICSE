469e1f73d507b1843220a4242e9354cb054fa212
KrithinP <76639820+KrithinP@users.noreply.github.com>
2021-11-10 06:47:28 +0530

    hw

--- src/com/company/HW1.java
+++ src/com/company/HW1.java
@@ -6,11 +6,11 @@ public class HW1 {
         int e =1;
         for (int i = 15; i >= 1; i -= 2)
         {
-            for (int j = i, f =e; (j >= 1)&&(1<=f); j -= 2)
+            for (int j = i, f =e; (1<=f); j -= 2)
             {
                 System.out.print(j + " ");
                 --f;
-                if(j==3)System.exit(0);
+
             }
             e++;
             System.out.println();
